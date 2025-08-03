5b69ae30d74303ed74345b15ac76c9bfff5c6e62
dixitthiya <dixitthiya@gmail.com>
2025-08-03 16:38:05 -0500

    Style upgrades, for mobile and pc screens

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ def quote():
     return render_template('index.html')  # This shows the form
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    app.run(host="0.0.0.0", port=10000)
